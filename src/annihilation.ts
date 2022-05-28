d9db6e511172bc8b77bdaa339303780b7a468b65
Krystian Prus <lamerhiob@gmail.com>
2022-05-28 19:49:50 +0200

    working

--- src/annihilation.ts
+++ src/annihilation.ts
@@ -1,5 +1,6 @@
 import './annihilation.scss';
 import html2canvas from 'html2canvas';
+import { toCanvas, toPng, toJpeg, toSvg } from 'html-to-image';
 
 export interface AnnihilationConfig {
     element: string | HTMLElement;
@@ -32,31 +33,64 @@ export type OnPartialAnimationEnd = (pieceCount: number, params: PartialParams) 
 
 
 export interface AnnihilationEnd {
-    element: HTMLElement
+    element: HTMLElement;
+}
+
+interface IGridSize {
+    columns: number;
+    rows: number;
+}
+
+function getElement(element: string | HTMLElement): HTMLElement {
+    return typeof element === 'string' ?
+        <HTMLElement>document.querySelector(element) :
+        element;
+}
+
+function getGridSize(element: HTMLElement, columns?: number, rows?: number): IGridSize {
+    let rect = element.getBoundingClientRect();
+
+    columns = columns || Math.round((rows || 0) * rect.width / rect.height) || 10;
+    rows = rows || Math.round(columns * rect.height / rect.width) || 10;
+
+    return { columns, rows };
+}
+
+function getCanvasByElementSize(element: HTMLElement): HTMLCanvasElement {
+    let rect = element.getBoundingClientRect();
+    let canvas: HTMLCanvasElement = document.createElement('canvas') as HTMLCanvasElement;
+    canvas.width = rect.width;
+    canvas.height = rect.height;
+
+    return canvas;
+}
+
+function elementToCanvas(element: HTMLElement): Promise<HTMLCanvasElement> {
+    return html2canvas(element, { backgroundColor: 'inherit', scale: 1 });
+}
+
+function elementToDataURL(element: HTMLElement): Promise<string> {
+    return elementToCanvas(element)
+        .then(function (canvas: HTMLCanvasElement) {
+            return canvas.toDataURL();
+        });
 }
 
 export function annihilation(config: AnnihilationConfig): Promise<AnnihilationEnd> {
     return new Promise(function (resolve, reject) {
-        const element: HTMLElement = typeof config.element === 'string' ?
-            <HTMLElement>document.querySelector(config.element) :
-            config.element;
+        const element: HTMLElement = getElement(config.element);
 
         if (element) {
-            let rect = element.getBoundingClientRect(),
-                columns: number = config.columns || 0,
-                rows: number = config.rows || 0;
+            elementToDataURL(element)
+                .then(function name(dataURL: string) {
+                    let { columns, rows } = getGridSize(element, config.columns, config.rows);
 
-            columns = columns || Math.round(rows * rect.width / rect.height) || 10;
-            rows = rows || Math.round(columns * rect.height / rect.width) || 10;
-
-            html2canvas(element, { backgroundColor: 'inherit' })
-                .then(function (canvas: HTMLCanvasElement) {
                     element.classList.add('annihilation');
 
                     let parent: HTMLElement = document.createElement('div');
                     parent.classList.add('annihilation__content');
 
-                    parent.style.backgroundImage = `url(${canvas.toDataURL()})`;
+                    parent.style.backgroundImage = `url(${dataURL})`;
                     parent.style.setProperty('--columns', columns.toString());
                     parent.style.setProperty('--rows', rows.toString());
 
@@ -134,4 +168,56 @@ export function annihilation(config: AnnihilationConfig): Promise<AnnihilationEn
             reject('Element not found');
         }
     });
+}
+
+
+
+
+function elementToCanvas2(element: HTMLElement): Promise<HTMLCanvasElement> {
+    return toCanvas(element, {
+        // backgroundColor: 'initial',
+        // pixelRatio: 1,//window.devicePixelRatio,
+        // width: rect.width,
+        // height: rect.height,
+        // canvasWidth: rect.width,
+        // canvasHeight: rect.height,
+        // style: {
+        //     margin: '0px',
+        //     padding: '0px'
+        // }
+    });
+}
+
+function elementToDataURL2(element: HTMLElement): Promise<string> {
+    return toSvg(element);
+    // return toPng(element);
+    // return toJpeg(element);
+}
+
+export function annihilation2(config: AnnihilationConfig): Promise<any> {
+    const element: HTMLElement = getElement(config.element);
+
+    return elementToCanvas(element)
+        .then(function name(canvas: HTMLCanvasElement) {
+            element.classList.add('annihilation');
+            canvas.classList.add('annihilation__content');
+            element.appendChild(canvas);
+
+            return element;
+        });
+}
+
+export function annihilation3(config: AnnihilationConfig): Promise<any> {
+    const element: HTMLElement = getElement(config.element);
+
+    return elementToDataURL(element)
+        .then(function name(dataUrl: string) {
+            element.classList.add('annihilation');
+            let img = new Image();
+            img.src = dataUrl;
+            img.classList.add('annihilation__content');
+            element.appendChild(img);
+
+            return element;
+        });
 }
