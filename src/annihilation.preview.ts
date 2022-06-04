04f1a8b4ec4288d69dd80ff8b45862360764e1e6
Krystian Prus <lamerhiob@gmail.com>
2022-06-04 14:33:30 +0200

    refactoring

--- src/annihilation.preview.ts
+++ src/annihilation.preview.ts
@@ -1,18 +1,31 @@
 import { AnnihilationBase } from './annihilation.base';
-import { IAnnihilationOptions } from './annihilation.models';
+import { IAfterAnnihilation, IAnnihilationOptions, IGridSize } from './annihilation.models';
 
 class AnnihilationPreview extends AnnihilationBase {
+    get GridSize(): IGridSize {
+        return {
+            columns: 0,
+            rows: 0
+        };
+    }
+
     renderTo(canvas: HTMLCanvasElement): HTMLElement | Promise<HTMLElement> {
         let { top, left, width, height } = this.PositionElement;
 
         canvas.classList.add('annihilation__content');
-        
+
         canvas.style.top = `${top}px`;
         canvas.style.left = `${left}px`;
         canvas.style.width = `${width}px`;
         canvas.style.height = `${height}px`;
 
         return canvas;
+    }
+
+    protected onEnd(): void {
+        this.emit<IAfterAnnihilation>('after-annihilation', {
+            element: this.element
+        });
     }
 }
 
